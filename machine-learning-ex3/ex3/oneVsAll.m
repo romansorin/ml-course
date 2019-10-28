942cb741fd191baa8d98fce0ae246976e501442a
Roman Sorin <988244@students.mentorschools.org>
2019-10-28 09:31:09 -0400

    Added token.mat to ignore

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -21,12 +21,12 @@ X = [ones(m, 1) X];
 % Instructions: You should complete the following code to train num_labels
 %               logistic regression classifiers with regularization
 %               parameter lambda. 
-%
+
 % Hint: theta(:) will return a column vector.
 %
 % Hint: You can use y == c to obtain a vector of 1's and 0's that tell you
 %       whether the ground truth is true/false for this class.
-%
+
 % Note: For this assignment, we recommend using fmincg to optimize the cost
 %       function. It is okay to use a for-loop (for c = 1:num_labels) to
 %       loop over the different classes.
@@ -48,16 +48,6 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
-
-
-
-
-
-
-
-
-
-
 
 
 % =========================================================================
